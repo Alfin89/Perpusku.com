f306d5d871d8b59e52f85bda0dee351cce7f9598
Alfin89 <rebahansantuy30@gmail.com>
2023-12-31 15:16:53 +0700

    Create Project Perpusku.com

---
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -0,0 +1,79 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="active">
+                    <a href="index.html"><img src="assets/img/icons/dashboard.svg" alt="img" /><span>
+                            Dashboard</span>
+                    </a>
+                </li>
+                <li class="submenu">
+                    <a href="javascript:void(0);"><img src="assets/img/icons/product.svg" alt="img" /><span>
+                            Kategori</span>
+                        <span class="menu-arrow"></span></a>
+                    <ul>
+                        <li>
+                            <a href="{{ url('category') }}">Daftar Kategori</a>
+                        </li>
+                        <li>
+                            <a href="addproduct.html">Tambah Kategori</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="javascript:void(0);"><img src="assets/img/icons/sales1.svg" alt="img" /><span>
+                            Buku-buku</span>
+                        <span class="menu-arrow"></span></a>
+                    <ul>
+                        <li>
+                            <a href="saleslist.html">Daftar Buku</a>
+                        </li>
+                        <li><a href="pos.html">Tambah Buku</a></li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="javascript:void(0);"><img src="assets/img/icons/users1.svg" alt="img" /><span>
+                            People</span>
+                        <span class="menu-arrow"></span></a>
+                    <ul>
+                        <li>
+                            <a href="userlist.html">User List</a>
+                        </li>
+                        <li><a href="adduser.html">Add User</a></li>
+                        <li>
+                            <a href="storelist.html">Store List</a>
+                        </li>
+                        <li>
+                            <a href="addstore.html">Add Store</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="submenu">
+                    <a href="javascript:void(0);"><img src="assets/img/icons/settings.svg" alt="img" /><span>
+                            Settings</span>
+                        <span class="menu-arrow"></span></a>
+                    <ul>
+                        <li>
+                            <a href="generalsettings.html">General Settings</a>
+                        </li>
+                        <li>
+                            <a href="emailsettings.html">Email Settings</a>
+                        </li>
+                        <li>
+                            <a href="paymentsettings.html">Payment Settings</a>
+                        </li>
+                        <li>
+                            <a href="currencysettings.html">Currency Settings</a>
+                        </li>
+                        <li>
+                            <a href="grouppermissions.html">Group Permissions</a>
+                        </li>
+                        <li>
+                            <a href="taxrates.html">Tax Rates</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
